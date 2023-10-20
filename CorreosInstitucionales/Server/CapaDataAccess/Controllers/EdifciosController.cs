8be94f1258c3450bda29fa8000fe0cfb005de353
EduardoCarrasco3 <ecarrascocastillo3@gmail.com>
2023-10-20 16:09:56 -0600

    Creacion de Controladores y ViewModels
    Edificios, TiposPersonal.

---
+++ CorreosInstitucionales/Server/CapaDataAccess/Controllers/EdifciosController.cs
@@ -0,0 +1,150 @@
+﻿using CorreosInstitucionales.Server.CapaDataAccess.DBContext;
+using CorreosInstitucionales.Shared.CapaEntities.ViewModels.Request;
+using CorreosInstitucionales.Shared.CapaEntities.ViewModels.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CorreosInstitucionales.Server.CapaDataAccess.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EdifciosController : Controller
+    {
+        [HttpGet("filterByStatus/{filterByStatus}")]
+        public async Task<IActionResult> GetAllData(bool filterByStatus)
+        {
+            Response<List<MceCatEdificio>> oResponse = new();
+
+            try
+            {
+                using (DbCorreosInstUpiicsaContext db = new())
+                {
+                    var list = new List<MceCatEdificio>();
+
+                    if (filterByStatus)
+                        list = await db.MceCatEdificios.Where(e => e.EdiStatus.Equals(filterByStatus)).ToListAsync();
+                    else
+                        list = await db.MceCatEdificios.ToListAsync();
+
+                    oResponse.Success = 1;
+                    oResponse.Data = list;
+                }
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+            }
+
+            return Ok(oResponse);
+        }
+
+        [HttpGet("filterById/{id}")]
+        public async Task<IActionResult> GetDataById(int id)
+        {
+            Response<MceCatEdificio> oResponse = new();
+
+            try
+            {
+                using (DbCorreosInstUpiicsaContext db = new())
+                {
+                    var list = await db.MceCatEdificios.FindAsync(id);
+                    oResponse.Success = 1;
+                    oResponse.Data = list;
+                }
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+            }
+
+            return Ok(oResponse);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddData(EdificioViewModel model)
+        {
+            Response<object> oResponse = new();
+
+            try
+            {
+                using (DbCorreosInstUpiicsaContext db = new())
+                {
+                    MceCatEdificio oEdificio = new()
+                    {
+                        IdEdificio = model.IdEdificio,
+                        EdiNombreOficial = model.EdiNombreOficial,
+                        EdiNombreAlias = model.EdiNombreAlias,
+                        EdiStatus = model.EdiStatus
+                    };
+                    await db.MceCatEdificios.AddAsync(oEdificio);
+                    await db.SaveChangesAsync();
+
+                    oResponse.Success = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+            }
+
+            return Ok(oResponse);
+        }
+
+        [HttpPut]
+        public IActionResult EditData(EdificioViewModel model)
+        {
+            Response<object> oRespuesta = new();
+
+            try
+            {
+                using DbCorreosInstUpiicsaContext db = new();
+                MceCatEdificio? oEdificio = db.MceCatEdificios.Find(model.IdEdificio);
+                if (oEdificio != null)
+                {
+                    oEdificio.EdiNombreOficial = model.EdiNombreOficial;
+                    oEdificio.EdiNombreAlias = model.EdiNombreAlias;
+                    oEdificio.EdiStatus = model.EdiStatus;
+
+                    db.Entry(oEdificio).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                }
+
+                oRespuesta.Success = 1;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Message = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpPut("editByIdStatus/{id}/{isActivate}")]
+        public IActionResult EnableDisableDataById(int id, bool isActivate)
+        {
+            Response<object> oRespuesta = new();
+
+            try
+            {
+                using DbCorreosInstUpiicsaContext db = new();
+                MceCatEdificio? oEdificio = db.MceCatEdificios.Find(id);
+                //db.Remove(oPersona);
+                if (oEdificio != null)
+                {
+                    oEdificio.EdiStatus = isActivate;
+                    db.Entry(oEdificio).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                oRespuesta.Success = 1;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Message = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+
+        }
+    }
+}
---
+++ CorreosInstitucionales/Shared/CapaEntities.ViewModels/Request/EdificioViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorreosInstitucionales.Shared.CapaEntities.ViewModels.Request
+{
+    public class EdificioViewModel
+    {
+        [Key]
+        public int IdEdificio { get; set; }
+
+        [Column("ediNombreOficial")]
+        [StringLength(200)]
+        public string EdiNombreOficial { get; set; } = null!;
+
+        [Column("ediNombreAlias")]
+        [StringLength(200)]
+        public string? EdiNombreAlias { get; set; }
+
+        [Column("ediStatus")]
+        public bool EdiStatus { get; set; }
+
+        //[InverseProperty("AreIdEdificioNavigation")]
+        //public virtual ICollection<MceCatAreasDepto> MceCatAreasDeptos { get; set; } = new List<MceCatAreasDepto>();
+    }
+}
