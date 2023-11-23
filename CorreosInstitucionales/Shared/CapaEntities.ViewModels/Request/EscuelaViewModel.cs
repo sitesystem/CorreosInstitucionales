410cd15ee7d727d151be919de50688b1885c8040
EduardoCarrasco3 <ecarrascocastillo3@gmail.com>
2023-11-23 16:24:24 -0600

    Catalogo escuelas

---
+++ CorreosInstitucionales/Shared/CapaEntities.ViewModels/Request/EscuelaViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorreosInstitucionales.Shared.CapaEntities.ViewModels.Request
+{
+    public class EscuelaViewModel
+    {
+        [Key]
+        public int IdEscuela { get; set; }
+
+        [Column("escNoEscuela")]
+        public int? EscNoEscuela { get; set; }
+
+        [Column("escNombreLargo")]
+        [StringLength(150)]
+        public string? EscNombreLargo { get; set; }
+
+        [Column("escNombreCorto")]
+        [StringLength(100)]
+        public string? EscNombreCorto { get; set; }
+
+        [Column("escLogo")]
+        [StringLength(100)]
+        public string? EscLogo { get; set; }
+
+        [Required]
+        [Column("escStatus")]
+        public bool? EscStatus { get; set; }
+    }
+}
