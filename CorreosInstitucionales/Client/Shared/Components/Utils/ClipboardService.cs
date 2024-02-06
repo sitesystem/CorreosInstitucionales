33ff29f5092308218954e5874bf4fd19940a3cff
Gabriel Mtz <gabrielmtzcarrillo@users.noreply.github.com>
2024-02-06 14:55:03 -0600

    Se mueven los archivos relacionados a la utilidad del portapapeles pt.2

---
+++ CorreosInstitucionales/Client/Shared/Components/Utils/ClipboardService.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace CorreosInstitucionales.Client.Shared.Components.Utils
+{
+    public class ClipboardService: IClipboardService
+    {
+        private readonly IJSRuntime _runtime;
+
+        public ClipboardService(IJSRuntime runtime) 
+        {
+            _runtime = runtime;
+        }
+
+        async Task IClipboardService.CopyToClipboard(string text)
+
+        {
+            await _runtime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+        }
+    }
+}
---
+++ CorreosInstitucionales/Client/Shared/Components/Utils/IClipboardService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CorreosInstitucionales.Client.Shared.Components.Utils
+{
+    public interface IClipboardService
+    {
+        Task CopyToClipboard(string text);
+    }
+}
