a4938a2296f322bfac23bd304441e12d580d89ab
Gabriel Mtz <gabrielmtzcarrillo@users.noreply.github.com>
2024-02-06 14:54:22 -0600

    Se mueve la utilidad de portapapeles de ubicación

--- CorreosInstitucionales/Client/Shared/Utils/IClipboardService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace CorreosInstitucionales.Client.Shared.Utils
-{
-    public interface IClipboardService
-    {
-        Task CopyToClipboard(string text);
-    }
-}
