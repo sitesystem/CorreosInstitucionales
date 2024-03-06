63daae429afeac44e337819a1601212af9c873c3
Gabriel Mtz <gabrielmtzcarrillo@users.noreply.github.com>
2024-03-06 16:10:10 -0600

    Constantes

---
+++ CorreosInstitucionales/Shared/Constantes/TipoEstadoSolicitud.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorreosInstitucionales.Shared.Constantes
+{
+    public static class TipoEstadoSolicitud
+    {
+        public const int ALTA               = 1;
+        public const int PENDIENTE          = 2;
+        public const int EN_PROCESO         = 3;
+        public const int ATENDIDA           = 4;
+        public const int ENCUESTA_CALIDAD   = 5;
+        public const int CANCELADA          = 6;
+    }
+}
