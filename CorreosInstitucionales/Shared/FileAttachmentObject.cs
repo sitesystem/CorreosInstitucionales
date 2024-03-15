40337631eb237428046ecf6f78ed40629a41357d
Gabriel Mtz <gabrielmtzcarrillo@users.noreply.github.com>
2024-03-15 17:04:24 -0600

    Clase auxiliar FileAttachmentObject para adjuntar archivos subidos a entidades
    Para actualizar registros que requieran actualizar o subir archivos

---
+++ CorreosInstitucionales/Shared/FileAttachmentObject.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorreosInstitucionales.Shared
+{
+    public class FileAttachmentObject<T>
+    {
+        public T Value { get; set; }
+        public Dictionary<int, IBrowserFile?>  Files { get; set; }
+
+        public FileAttachmentObject(T v, IBrowserFile? f, int filetype)
+        {
+            Value = v;
+            Files = new();
+        }
+
+        public FileAttachmentObject(T v, Dictionary<int, IBrowserFile?> files)
+        {
+            Value = v;
+            Files = files;
+        }
+    }
+}
