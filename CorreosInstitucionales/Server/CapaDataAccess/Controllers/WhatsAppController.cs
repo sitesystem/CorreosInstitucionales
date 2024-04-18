fa276d4d2e2cbbe0794c3b7923e3ba40fe65f8e6
sitesystem <l_aloaxa05@hotmail.com>
2024-04-18 12:21:23 -0600

    Restaurar Controlador WhatsApp

---
+++ CorreosInstitucionales/Server/CapaDataAccess/Controllers/WhatsAppController.cs
@@ -0,0 +1,44 @@
+﻿using CorreosInstitucionales.Shared.CapaEntities.Request;
+using CorreosInstitucionales.Shared.CapaEntities.Response;
+using CorreosInstitucionales.Shared.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Runtime.ConstrainedExecution;
+using System.Text.Json;
+
+namespace CorreosInstitucionales.Server.CapaDataAccess.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class WhatsAppController(HttpClient _client): Controller
+    {
+        const string url = "https://www.developers.upiicsa.ipn.mx:8081/api/SendWhatsApp";
+
+        protected readonly HttpClient client = _client;
+        protected readonly JsonSerializerOptions _options = new() { PropertyNameCaseInsensitive = true };
+        
+        [HttpPost("send")]
+        public async Task<IActionResult> WA_Send(RequestDTO_SendWhatsApp message)
+        {
+            Response<string> oResponse = new();
+
+            if(message.Number == "5500000000")
+            {
+                oResponse.Success = 1;
+                oResponse.Data = "EL MENSAJE NO SE ENVIÓ DADO QUE ES UN NÚMERO DE PRUEBA.";
+                return Ok(oResponse);
+            }
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(url, message, options: _options);
+
+            if(response.IsSuccessStatusCode)
+                oResponse.Success = 1;
+
+            oResponse.Data = await response.Content.ReadAsStringAsync();
+
+            return Ok(oResponse);
+        }
+    }
+}
