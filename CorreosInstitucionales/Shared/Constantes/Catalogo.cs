5ae72c23fbed1303abbdc71288a633a8c13759a0
Gabriel Mtz <gabrielmtzcarrillo@users.noreply.github.com>
2024-04-29 15:07:44 -0600

    Se actualiza el editor del catálogo de usuarios
    Se agrega una lista desplegable de semestres y se cambia el correo por defecto de @localhost a @example.com, para que no se muestre el mensaje de error que no es un nombre de dominio válido.

---
+++ CorreosInstitucionales/Shared/Constantes/Catalogo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorreosInstitucionales.Shared.Constantes
+{
+    public static class Catalogo
+    {
+        public static TDataItem<string>[] Semestres = 
+        [
+            new ("1","Primer"),
+            new ("2","Segundo"),
+            new ("3","Tercer"),
+            new ("4","Cuarto"),
+            new ("5","Quinto"),
+            new ("6","Sexto"),
+            new ("7","Séptimo"),
+            new ("8","Octavo"),
+        ];
+    }
+}
