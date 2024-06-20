a32f7aede0099b14b684347430678b3aabe58942
Gabriel Mtz <gabrielmtzcarrillo@users.noreply.github.com>
2024-06-20 14:12:56 -0600

    7z para la generación del archivo de importación al VPS

--- generar_zip.bat
+++ generar_zip.bat
@@ -3,6 +3,11 @@ SETLOCAL
 
 SET "publish_path=.\CorreosInstitucionales\Server\bin\Release\net8.0\publish"
 
+ECHO ELIMINANDO LOGS
+ECHO %publish_path%\wwwroot\logs\
+DEL %publish_path%\wwwroot\logs\*.txt
+PAUSE
+
 ECHO COPIANDO ARCHIVO DE CONFIGURACIÓN...
 COPY %publish_path%\appsettings.production.json %publish_path%\appsettings.json /V /Y
 ECHO.
@@ -14,7 +19,7 @@ IF EXIST publish.zip (
 )
 
 ECHO GENERANDO PUBLISH.ZIP
-.\software\7zr a publish.zip -x^!publish\wwwroot\repositorio -mx0 %publish_path%
+.\software\7zip\7za.exe a publish.zip -x^!publish\wwwroot\repositorio -mx0 %publish_path%
 
 
 ::tar -cvf publish.zip .\CorreosInstitucionales\Server\bin\Release\net8.0\publish\*
