e98641dc246293544a07f037d45996227d692691
Gabriel Mtz <gabrielmtzcarrillo@users.noreply.github.com>
2024-09-26 13:46:22 -0600

    UI

--- CorreosInstitucionales/Shared/CapaEntities/Request/RequestDTO_FinalizarSolicitud.cs
+++ CorreosInstitucionales/Shared/CapaEntities/Request/RequestDTO_FinalizarSolicitud.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CorreosInstitucionales.Shared.Constantes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,6 @@ namespace CorreosInstitucionales.Shared.CapaEntities.Request
     {
         public int IdSolicitud { get; set; }
         public string? Mensaje { get; set; }
+        public int Estado { get; set; }
     }
 }
