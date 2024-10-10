145ad707c2f139985982defb5fab1c5e51a88fc1
Gabriel Mtz <gabrielmtzcarrillo@users.noreply.github.com>
2024-10-10 13:42:41 -0600

    Clases para manejar archivos seleccionados desde modales

---
+++ CorreosInstitucionales/Shared/ContentData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorreosInstitucionales.Shared
+{
+    public class ContentData
+    {
+        public string ContentType { get; set; } = "";
+        public byte[] Bytes { get; set; } = [];
+        public string FileName { get; set; } = "UNDEFINED";
+    }
+}
---
+++ CorreosInstitucionales/Shared/FileAttachment.cs
@@ -0,0 +1,41 @@
+﻿using CorreosInstitucionales.Shared.Constantes;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorreosInstitucionales.Shared
+{
+    public class FileAttachment<T>
+    {
+        public T Value { get; set; }
+        public List<ContentData> Attachments { get; set; } = new();
+        public FileAttachment(T v)
+        {
+            Value = v;
+        }
+
+        public async Task AttachFile(IBrowserFile file)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (var fs = file.OpenReadStream(file.Size))
+                {
+                    await fs.CopyToAsync(ms);
+                }
+
+                Attachments.Add(new()
+                {
+                    ContentType = file.ContentType,
+                    FileName = file.Name,
+                    Bytes = ms.ToArray()
+                });
+            }
+        }
+    }
+}
