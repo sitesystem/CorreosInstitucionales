a6e81b4c233be37666b947a202a0c3903fa7d7d2
Gabriel Mtz <gabrielmtzcarrillo@users.noreply.github.com>
2024-10-14 13:58:40 -0600

    Cambios GUI
    - Se agrega popup de mensaje de pantalla de carga
    - Se simplifica el modal de cancelación/finalización de solicitud
    - Se agrega la opción de evitar que los archivos adjuntos se descarguen automáticamente

--- CorreosInstitucionales/Shared/CapaTools/Notificacion.cs
+++ CorreosInstitucionales/Shared/CapaTools/Notificacion.cs
@@ -1,6 +1,9 @@
 ﻿using CorreosInstitucionales.Shared.CapaEntities.Request;
+using CorreosInstitucionales.Shared.CapaEntities.Response;
 using CorreosInstitucionales.Shared.CapaServices.BusinessLogic.toolSendEmail;
 using CorreosInstitucionales.Shared.Constantes;
+using CorreosInstitucionales.Shared.Utils;
+using Microsoft.AspNetCore.Components.RenderTree;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +35,39 @@ namespace CorreosInstitucionales.Shared.CapaTools
         public bool EsWAPrueba()
         {
             return wa.Number == "5500000000";
+        }
+
+        public async Task<List<string>> Enviar(ISendEmailService servicioCorreo, WhatsApp WA)
+        {
+            string id = Guid.NewGuid().ToString();
+            string archivo = $"{ServerFS.GetBaseDir(true)}/repositorio/envios/{id}.txt";
+
+            List<string> log = [];
+
+            if (EsCorreoPrueba())
+            {
+                log.Add($"[CORREO DE PRUEBA] {correo.EmailTo}");
+            }
+            else
+            {
+                _ = Task.Run(() => servicioCorreo.SendEmailAsync(correo));
+                log.Add($"[CORREO ENVIADO:] {correo.EmailTo}");
+            }
+
+            if (EsWAPrueba())
+            {
+                log.Add($"[WA DE PRUEBA] {wa.Message}");
+            }
+            else
+            {
+                log.Add($"[ENVIO DE WA] {correo.EmailTo}");
+                Response<string> response = await WA.SendMessage(wa);
+                log.Add(response.Success == 1 ? "[OK]" : "[ERROR]");
+            }
+            
+            System.IO.File.WriteAllText(archivo, string.Join(Environment.NewLine, log));
+
+            return log;
         }
     }
 }
