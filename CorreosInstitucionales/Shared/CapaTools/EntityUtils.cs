49e2ecf94f1f6b7b326ec9acb4eceda218f239fe
Gabriel Mtz <gabrielmtzcarrillo@users.noreply.github.com>
2024-10-18 15:36:26 -0600

    Se combina TemplateEntity en EnityUtils y se plican cambios

--- CorreosInstitucionales/Shared/CapaTools/EntityUtils.cs
+++ CorreosInstitucionales/Shared/CapaTools/EntityUtils.cs
@@ -1,5 +1,6 @@
 ﻿using CorreosInstitucionales.Shared.CapaDataAccess.DBContext;
 using CorreosInstitucionales.Shared.CapaEntities.Request;
+using CorreosInstitucionales.Shared.Constantes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +28,57 @@ namespace CorreosInstitucionales.Shared.CapaTools
             }
 
             return result;
+        }
+
+        /**************************  CREACION DE ELEMENTOS  *****************************/
+        public static RequestDTO_Usuario DefaultUsuario()
+        {
+            return new RequestDTO_Usuario()
+            {
+                UsuIdRol = 2,
+                UsuIdTipoPersonal = 1,
+                UsuCurp = "XAXX010101HDFXXX00",
+                UsuBoletaAlumno = "0000600000",
+                UsuBoletaMaestria = "B000000",
+                UsuCorreoPersonalCuentaNueva = "noreply@example.com",
+                UsuContrasenia = Encrypt.GetSHA256(Guid.NewGuid().ToString()),
+                UsuNoCelularNuevo = "55 00 00 00 00",
+                UsuIdCarrera = 1,
+                UsuSemestre = "1",
+                UsuAnioEgreso = 1950,
+                UsuFileNameComprobanteInscripcion = "-",
+                UsuNumeroEmpleado = "0",
+                UsuIdAreaDepto = 1,
+                UsuNoExtension = "0",
+                UsuStatus = true
+            };
+        }
+
+        public static RequestDTO_Solicitud DefaultSolicitud()
+        {
+            return new RequestDTO_Solicitud()
+            {
+                SolFileNameCurp = "-",
+                SolFileNameComprobanteInscripcion = "-",
+                SolNoCelularAnterior = "-",
+                SolNoCelularActual = "-",
+                SolCorreoPersonalCuentaAnterior = "-",
+                SolCorreoPersonalCuentaActual = "-",
+                SolCapturaEscaneoAntivirus = "-",
+                SolCapturaCuentaBloqueada = "-",
+                SolCapturaError = "-",
+                SolObservacionesSolicitud = "-",
+                SolIdEstadoSolicitud = (int)TipoEstadoSolicitud.PENDIENTE
+            };
+        }
+
+        public static RequestViewModel_Anuncio DefaultAnuncio()
+        {
+            return new RequestViewModel_Anuncio()
+            {
+                AnuArchivo = "-",
+                AnuStatus = true
+            };
         }
     }
 }
