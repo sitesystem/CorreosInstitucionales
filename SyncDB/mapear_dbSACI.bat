58d352b7ace34f47803520073334d4bc8ab94271
Gabriel Mtz <gabrielmtzcarrillo@users.noreply.github.com>
2024-11-20 11:37:19 -0600

    Se agrega la actualización de la versión de .NET y se especifica que no s se envien estadísticas de uso

--- SyncDB/mapear_dbSACI.bat
+++ SyncDB/mapear_dbSACI.bat
@@ -1,4 +1,5 @@
 SETLOCAL
+SET DOTNET_CLI_TELEMETRY_OPTOUT=1
 ::https://learn.microsoft.com/en-us/ef/core/managing-schemas/scaffolding/?tabs=dotnet-core-cli
 
 ::dotnet add package Microsoft.EntityFrameworkCore.SqlServer
@@ -9,6 +10,8 @@ SETLOCAL
 ::-Provider Microsoft.EntityFrameworkCore.SqlServer ^
 ::"SQLServer_Connection": "Server=www.developers.upiicsa.ipn.mx;Database=db_CorreosInstitucionales_UPIICSA;User ID=correos_institucionales;Password=correos_institucionales;Trusted_Connection=False;TrustServerCertificate=True;",
 ::"Docker_Connection": "Server=148.204.211.186;Database=db_Centralizada_UPIICSA;User ID=sa;Password=#Password!!1;Trusted_Connection=False;TrustServerCertificate=True;"
+
+dotnet tool update --global dotnet-ef
 
 dotnet ef dbcontext scaffold ^
 	"Server=www.developers.upiicsa.ipn.mx;Database=db_CorreosInstitucionales_UPIICSA;User ID=correos_institucionales;Password=correos_institucionales;Trusted_Connection=False;TrustServerCertificate=True;" ^
